91b2720ba6f35513ccb5a65db79b8b672a4ba84a
pohar <poharhd@gmail.com>
2023-12-01 19:13:30 +0100

    Day1 Pt1 Pt2

---
+++ main.py
@@ -0,0 +1,106 @@
+def Findfirstdigit(str1):
+    ret=0
+
+    for c in str1:
+        if c.isdigit():
+            ret=int(c)
+            break
+
+    return ret
+
+def Findlastdigit(str2):
+    ret=0
+
+    for c in str2:
+        if c.isdigit():
+            ret=int(c)
+    return ret
+
+
+def Findfirstdigitname(str1):
+    ret=0
+
+    nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    foundnum = -1
+    foundpos = 99999
+    for i in range(len(str1)):
+        p=1
+        for num in nums:
+            x = str1[i:].startswith(num)
+            if x:
+                if i<foundpos:
+                    foundnum = p
+                    foundpos = i
+                    ret = int(foundnum)
+                break
+            p = p + 1
+
+    p2=0
+    for c in str1:
+        if c.isdigit():
+            if p2<foundpos:
+                ret=int(c)
+            else:
+                ret = int(foundnum)
+            break
+        p2 = p2 + 1
+
+    return ret
+
+def Findlastdigitname(str2):
+    ret=0
+
+    nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    foundnum = -1
+    foundpos = -1
+    for i in range(len(str2)):
+        p=1
+        for num in nums:
+            x = str2[i:].startswith(num)
+            if x:
+                if i>foundpos:
+                    foundnum = p
+                    foundpos = i
+                    ret = int(foundnum)
+            p = p + 1
+
+    p2=0
+    for c in str2:
+        if c.isdigit():
+            if p2>foundpos:
+                ret=int(c)
+            else:
+                ret = int(foundnum)
+        p2 = p2 + 1
+    return ret
+
+def Day1Pt1(inputfile):
+    print('Day 1 Part 1')
+    sum = 0
+    for line in open(inputfile):
+
+        res = Findfirstdigit(line) *10 + Findlastdigit(line)
+        print(f'Line res: {line} {res}')
+        sum = sum+res
+
+    print(f"Sum: {sum}")
+
+def Day1Pt2(inputfile):
+    print('Day 1 Part 1')
+    sum = 0
+    for line in open(inputfile):
+
+        res = Findfirstdigitname(line) *10 + Findlastdigitname(line)
+        print(f'Line res: {line} {res}')
+        sum = sum+res
+
+    print(f"Sum: {sum}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #Day1Pt1('input1_.txt')
+    Day1Pt2('input1.txt')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
